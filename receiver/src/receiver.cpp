8023fa1154b69f1aaec12e2b9bae54d19ea9bd63
gautam-sh <gtmsharma70@gmail.com>
2022-10-19 15:58:49 +0530

    add extract data method

--- receiver/src/receiver.cpp
+++ receiver/src/receiver.cpp
@@ -3,17 +3,34 @@
 #include <string>
 #include "../test/test_receiver.hpp"
 
-void readConsole()
+#include <algorithm>
+// #include "statistics_handler.hpp"
+
+std::vector<std::string> readConsole()
 {
     std::cout << "Waiting for input\n";
     std::string input_stream;
+    std::vector<std::string> received_data;
+    std::string delimiter = ",";
     while (std::getline(std::cin, input_stream))
     {
+        received_data.push_back(input_stream.substr(0, input_stream.find(delimiter)));
         std::cout << "Received line: " << input_stream << std::endl;
     }
+
+    return received_data;
+}
+
+void extractSensorData(std::vector<std::string> input)
+{
+    std::vector<StatisticsHandler> sensor_data;
 }
 
 int main()
 {
-    readConsole();
+    std::vector<std::string> console_input{readConsole()};
+    for(auto it : console_input)
+    {
+        std::cout << it << std::endl;
+    }
 }
